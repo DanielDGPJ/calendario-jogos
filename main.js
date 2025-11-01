05f137d8968e36d4f7f1ede0bd9eb6faa14af456
DanielDGPJ <ivo.d.pimenta@dgpj.mj.pt>
2025-11-01 06:50:59 +0000

    Update main.js

--- main.js
+++ main.js
@@ -191,7 +191,7 @@ function atualizarCalendario() {
     const isento = ["0000"].includes(dados.equipas.find(e => e.id === jogo.equipacasa)?.clubeId) ||
                    ["0000"].includes(dados.equipas.find(e => e.id === jogo.equipafora)?.clubeId);
 
-    let corFundo = "#e0e0e0";
+    let corFundo = "#f2f2f2";
     if (isento) {
       corFundo = dados.clubes.find(c => c.id === 0)?.cor || "#e8e8e8";
     } else if (destacar) {
@@ -225,4 +225,5 @@ function atualizarCalendario() {
     `;
   }
 }
+
 
